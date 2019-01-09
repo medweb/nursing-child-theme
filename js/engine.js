7d09057c7cff62714f318888497e27e48cf3b6f5
Stephen Schrauger <stephen@schrauger.com>
2019-01-09 10:58:09 -0500

    remove buggy sliding tabs

--- js/engine.js
+++ js/engine.js
@@ -3,15 +3,15 @@ jQuery( document ).ready(function($) {
     $('.toggle').click(function(event, element){
         var section_name = $(this).data('id');
         //console.log(section_name);
-        $('.menu-expanded[for="' + section_name + '"]').slideToggle("medium");
-        $('.menu-expanded[for!="' + section_name + '"]').slideUp("medium");
+        $('.menu-expanded[for="' + section_name + '"]').show();
+        $('.menu-expanded[for!="' + section_name + '"]').hide();
         event.preventDefault();
     });
     $('select.mobile').on('change', function(event, element){
         var section_name = $(this).find(":checked").data('id');
         //console.log(section_name);
-        $('.menu-expanded[for="' + section_name + '"]').slideToggle("medium");
-        $('.menu-expanded[for!="' + section_name + '"]').slideUp("medium");
+        $('.menu-expanded[for="' + section_name + '"]').show();
+        $('.menu-expanded[for!="' + section_name + '"]').hide();
         event.preventDefault();
     });
     // have the first section be pre-expanded
