754429142462b7dc6f7f9daf417f0f234fc45106
mattyvac <mattyvac@gmail.com>
2019-02-16 11:41:18 -0500

    removed commente dout static code

--- template-basic.php
+++ template-basic.php
@@ -47,49 +47,4 @@ if (get_field('enable_top_menu') && have_rows('inter_nav')) {
 	</article>
 </div>
 
-<?php if (is_front_page()) { ?>
-
-
-
-	<!-- <div class="container">
-
-		<h2>Choose Path Boxes</h2>
-
-		<div class="grid-box">
-
-			<a class="" href=""><span>Text</span></a>
-			<a class="" href=""><span>Text</span></a>
-			<a class="" href=""><span>Text</span></a>
-			<a class="" href=""><span>Text</span></a>
-			<a class="" href=""><span>Text</span></a>
-			<a class="" href=""><span>Text</span></a>
-
-		</div>
-
-	</div>
-
-	<div class="wide-box">
-
-		<div class="wide-box-overlay"></div>
-
-		<div class="container">
-		
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-
-			<div class="inner-box"><img src="https://www.ucf.edu/files/2017/11/College-Pages-icons-Trophy.png" /><br/>Bachelor of Science in Nursing (BSN), Master of Science in Nursing (MSN), Doctor of Nursing Practice (DNP) and APRN Certificate programs are nationally accredited by the Commission of Collegiate Nursing Education.</div>
-		
-		</div>
-
-	</div> -->
-
-<?php } ?>
-
 <?php get_footer(); ?>
