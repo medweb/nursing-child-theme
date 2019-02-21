3ace632ed3b6209953910a1139f09f0e079fa55a
mattyvac <mattyvac@gmail.com>
2019-02-21 13:45:42 -0500

    lots of changes to basic template and site wide styles from last meeting with nursing

--- template-basic.php
+++ template-basic.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Template Name: Basic
- * Template Post Type: degree
  */
 ?>
 
 <?php get_header(); the_post(); ?>
+
 <?php
 if (get_field('enable_top_menu') && have_rows('inter_nav')) {
     echo "<ul class='inter-nav'><div class='container'>";
@@ -24,7 +24,12 @@ if (get_field('enable_top_menu') && have_rows('inter_nav')) {
 <div class="container mb-5 mt-3 mt-lg-5">
 
 	<article class="<?php echo $post->post_status; ?> post-list-item">
+	
 		<?php
+		//news meta
+		if ( is_single() ) { echo '<small>Released on '.get_the_date(),'</small>'; }
+
+
 		// show side nav menu if defined for page
 		if (get_field('enable_side_menu') && have_rows('extra_nav')) {
 
@@ -42,7 +47,49 @@ if (get_field('enable_top_menu') && have_rows('inter_nav')) {
 		}
 		?>
 
-		<?php the_content(); ?>
+		<?php the_content(); 
+
+		if ( is_single() ) { ?>
+
+			<div class="interest-content">
+
+				<h5>Has the UCF College of Nursing Positively Impacted You?</h5>
+
+				<p>The mission of the College of Nursing at the University of Central Florida is to prepare nurse leaders and patient advocates through excellence in education, research and service.</p>
+
+				<a href="<?php bloginfo( 'url' ); ?>/giving/">Find out how to give back to your college, today!</a>
+
+			</div>
+
+			<h2>More Stories</h2>
+
+			<section class="triple-box-container">
+
+			<?php
+
+			//TODO find curent sub meta taxonomy and pull only stories from it in this array query
+
+	        $args = array(
+	            'post_type' => 'post',
+	            'posts_per_page' => '3'
+			);
+	        
+	        $the_query = new WP_Query( $args );
+	        
+	        while ( $the_query->have_posts() ) : $the_query->the_post(); 
+
+	        $preview = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+			$image = $preview[0]; ?>
+
+			<div class="triple-box"><a href="<?php the_permalink(); ?>" style="background: transparent url('<?php echo $image; ?>') no-repeat center center; background-size: cover;"><span><?php the_title(); ?></span></a></div>
+
+			<?php endwhile; wp_reset_postdata(); ?>
+
+		</section>
+
+		<?php } ?>
+
+
 
 	</article>
 </div>
