834bc0da542018a9ff217273aadf18dc797f5585
Stephen Schrauger <stephen@schrauger.com>
2019-03-22 15:18:29 -0400

    adds class to new link

--- js/view-all-events.js
+++ js/view-all-events.js
@@ -7,6 +7,10 @@ jQuery( document ).ready(function($) {
     /*
     Add a link to view more events
      */
-    $('.ucf-events .ucf-events-list').append("<div class='ucf-event ucf-event-row'><a href='https://events.ucf.edu/calendar/90/events-at-ucf-college-of-nursing/'>View all events</a></div>")
+    $('.ucf-events .ucf-events-list').append(
+        "<div class='ucf-event ucf-event-row'>" +
+        "<a class='view-all-events' href='https://events.ucf.edu/calendar/90/events-at-ucf-college-of-nursing/'>View all events</a>" +
+        "</div>"
+    )
 
 });
