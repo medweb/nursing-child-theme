9ffb6439a7300c0fca3aa97c5d7c59ca55811a04
Stephen Schrauger <stephen@schrauger.com>
2019-03-22 22:38:05 -0400

    link to upcoming filter

--- js/view-all-events.js
+++ js/view-all-events.js
@@ -9,7 +9,7 @@ jQuery( document ).ready(function($) {
      */
     $('.ucf-events .ucf-events-list').append(
         "<div class='ucf-event ucf-event-row'>" +
-        "<a class='view-all-events' href='https://events.ucf.edu/calendar/90/events-at-ucf-college-of-nursing/'>View all events</a>" +
+        "<a class='view-all-events' href='https://events.ucf.edu/calendar/90/events-at-ucf-college-of-nursing/upcoming/'>View all events</a>" +
         "</div>"
     )
 
