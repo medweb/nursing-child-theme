39c4569a934de9a8cbe9bbd2065d6208e0eeb349
Stephen Schrauger <stephen@schrauger.com>
2019-04-05 16:11:01 -0400

    pagination for news archive

--- template-archive.php
+++ template-archive.php
@@ -22,3 +22,8 @@
 </div>
 
 <?php endwhile; ?>
+
+<div class="pagination">
+    <div class="nav-next alignright"><?php next_posts_link( 'Older posts' ); ?></div>
+    <div class="nav-previous alignleft"><?php previous_posts_link( 'Newer posts' ); ?></div>
+</div>
